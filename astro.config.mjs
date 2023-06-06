85f9a0c23d8bcf8dc9a43ae11a7b9140091893c0
David Figueroa <davidfigueroa0327@gmail.com>
2023-06-06 01:17:02 -0500

    get site from cf variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,10 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 import { loadEnv } from "vite";
-const { CF_PAGES_URL, CF_PAGES, CF_PAGES_BRANCH, CUSTOM_DOMAIN } = loadEnv(
-  import.meta.env.MODE,
-  process.cwd(),
-  ""
-);
+const { CF_PAGES_URL } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 const getSite = () => {
-  if (CUSTOM_DOMAIN) {
-    return CUSTOM_DOMAIN;
-  } else if (CF_PAGES && !["master", "main"].includes(CF_PAGES_BRANCH)) {
+  if (CF_PAGES_URL) {
     return CF_PAGES_URL;
   } else {
     return "http://localhost:8788";
