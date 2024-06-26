822a27cb14e98a89777d0018560fdc2e9f602d08
David Figueroa <davidfigueroa0327@gmail.com>
2024-06-25 22:31:55 -0500

    remove squile warning

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module DaysSinceMidzu
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.active_record.sqlite3_production_warning=false
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
