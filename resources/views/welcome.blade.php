8d9e56c49411586449694f9ab9f159193fc462b7
David Figueroa <davidfigueroa0327@gmail.com>
2024-10-23 15:38:20 -0500

    make landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,66 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
 
-    </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        Days since Midzui
-    </body>
+<head>
+    <style>
+        body {
+            width: 100dvw;
+            height: 100dvh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            background-color: #ad8dda;
+            color: #fff;
+            margin: 0;
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif
+        }
+
+        h1 {
+            font-size: 3rem;
+            margin-bottom: 6rem
+        }
+
+        .elements {
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: auto 1fr;
+            gap: 2rem
+        }
+
+        .title {
+            font-size: larger;
+            text-align: center;
+            grid-row: 2 / 3;
+            margin: auto 0;
+            padding-left: 1rem;
+            padding-right: 1rem
+        }
+
+        .element {
+            font-size: 4rem;
+            font-weight: 600;
+            text-align: center;
+            margin-top: 1rem;
+            margin-bottom: 1rem
+        }
+
+        .dijo.title {
+            max-width: 20rem
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Han pasado</h1>
+    <div class="elements">
+        <p class="dijo element">{{ $said }}</p>
+        <h2 class="dijo title">
+            días desde que Midzui dijo que volvía "esta vez para siempre"
+        </h2>
+        <p class="stream element">{{ $last_stream }}</p>
+        <h2 class="stream title">días desde el último stream</h2>
+    </div>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Record;
+use Carbon\Carbon;
 
 Route::get('/', function () {
-    return view('welcome');
+    $record = Record::find(1);
+    $last_stream_days = floor(Carbon::parse($record->last_stream)->diffInDays());
+    $said_days = floor(Carbon::parse($record->said)->diffInDays());
+
+    return view('welcome', ['last_stream' => $last_stream_days, 'said' => $said_days]);
 });
